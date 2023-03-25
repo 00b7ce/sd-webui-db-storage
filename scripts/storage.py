d31f0fd531633750a52df64d14d2442facddc20b
00b7ce <sleep.seep.sheep@gmail.com>
2023-03-25 14:36:25 +0900

    move setting form and add record info. - move setting form -> setting form in Settings tab. - add record info -> Add Hires fix info

--- scripts/storage.py
+++ scripts/storage.py
@@ -6,6 +6,8 @@ import modules.scripts as scripts
 import gradio as gr
 from pymongo import MongoClient
 
+from modules import script_callbacks, shared
+
 mongo_host = os.environ.get('DB_HOST', 'localhost')
 mongo_port = int(os.environ.get('DB_PORT', 27017))
 mongo_username = os.environ.get('DB_USER', '')
@@ -15,9 +17,9 @@ creds = f"{mongo_username}:{mongo_password}@" if mongo_username and mongo_passwo
 client = MongoClient(f"mongodb://{creds}{mongo_host}:{mongo_port}/")
 
 
-def get_collection(database_name, collection_name):
-    db = client[database_name]
-    collection = db[collection_name]
+def get_collection():
+    db = client[shared.opts.data.get("db_strage_database_name")]
+    collection = db[shared.opts.data.get("db_strage_collection_name")]
     return collection
 
 class Scripts(scripts.Script):
@@ -27,50 +29,47 @@ class Scripts(scripts.Script):
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
-    def ui(self, is_img2img):
-        checkbox_save_to_db = gr.inputs.Checkbox(label="Save to DB", default=False)
-        database_name = gr.inputs.Textbox(label="Database Name", default="StableDiffusion")
-        collection_name = gr.inputs.Textbox(label="Collection Name", default="Automatic1111")
-        return [checkbox_save_to_db, database_name, collection_name]
-
-    def postprocess(self, p, processed,checkbox_save_to_db,database_name,collection_name):
-        collection = get_collection(database_name, collection_name) if checkbox_save_to_db else None
+    def postprocess(self, p, processed):
+        is_save = shared.opts.data.get("db_strage_is_save_db")
+        collection = get_collection() if is_save else None
         if collection is None:
             return True
         
         for i in range(len(processed.images)):
-
-            # Extract image information
-            regex = r"Steps:.*$"
-            seed = processed.seed
-            prompt = processed.prompt
-            neg_prompt = processed.negative_prompt
-            info = re.findall(regex, processed.info, re.M)[0]
+            info = re.findall(r"Steps:.*$", processed.info, re.M)[0]
             input_dict = dict(item.split(": ") for item in str(info).split(", "))
-            steps = int(input_dict["Steps"])
-            seed = int(input_dict["Seed"])
-            sampler = input_dict["Sampler"]
-            cfg_scale = float(input_dict["CFG scale"])
-            size = tuple(map(int, input_dict["Size"].split("x")))
-            model_hash = input_dict["Model hash"]
-            model = input_dict["Model"]
 
             image = processed.images[i]
             buffer = BytesIO()
             image.save(buffer, "png")
             image_bytes = buffer.getvalue()
 
-            if checkbox_save_to_db:
-                collection.insert_one({
-                    "prompt": prompt, 
-                    "negative_prompt": neg_prompt, 
-                    "steps": int(steps), 
-                    "seed": int(seed), 
-                    "sampler": sampler,
-                    "cfg_scale": float(cfg_scale), 
-                    "size": size, 
-                    "model_hash": model_hash, 
-                    "model": model, 
-                    "image": image_bytes
-                })
+            collection.insert_one({
+                "prompt": processed.prompt, 
+                "negative_prompt": processed.negative_prompt, 
+                "steps": int(input_dict["Steps"]), 
+                "seed": int(processed.seed), 
+                "sampler": input_dict["Sampler"],
+                "Denoising strength": float(input_dict["Denoising strength"]),
+                "Hires upscale": int(input_dict["Hires upscale"]),
+                "Hires steps": int(input_dict["Hires steps"]),
+                "Hires upscaler": input_dict["Hires upscaler"],
+                "cfg_scale": float(input_dict["CFG scale"]), 
+                "model": input_dict["Model"],
+                "model_hash": input_dict["Model hash"],
+                "size": tuple(map(int, input_dict["Size"].split("x"))), 
+                "rate": 0,
+                "image": image_bytes
+            })
         return True
+
+def on_ui_settings():
+    section = ('db_storage', "DB Strage")
+    # Save to DB for all generated images
+    shared.opts.add_option("db_strage_is_save_db", shared.OptionInfo(False, "Save to DB for all generated images", gr.Checkbox, {"interactive": True}, section=section))
+    # Database Name
+    shared.opts.add_option("db_strage_database_name", shared.OptionInfo('StableDiffusion', "Database Name", section=section))
+    # Collection name
+    shared.opts.add_option("db_strage_collection_name", shared.OptionInfo('Automatic1111', "Collection name", section=section))
+
+script_callbacks.on_ui_settings(on_ui_settings)
